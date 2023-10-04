e3f924ed2cfd092572ef9f37bd36371d7a36878a
Shreesha M <shreesha2k22@yahoo.com>
2023-10-04 22:22:42 +0530

    added alzheimer screen and updated database to store name,age,sex

--- api/app.py
+++ api/app.py
@@ -134,12 +134,69 @@ def predict_xray():
         return jsonify({'error': str(e)}), 500
 
 
+def predict_alzheimer():
+    try:
+        class_labels = []
+
+        with open('dementia_label.txt', 'r') as file:
+            for line in file:
+                parts = line.strip().split()
+                if len(parts) > 1:
+                    class_labels.append(' '.join(parts[1:]))
+
+        model = load_model('dementia_model.h5')
+        # Receive image data as base64 and decode it.
+        data = request.json.get('image_bytes')
+        image_bytes = base64.b64decode(data)
+
+        # Convert to PIL Image.
+        image = Image.open(BytesIO(image_bytes))
+
+        data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+        # Preprocess the image as needed for your model.
+        # Example:
+        if image.mode != 'RGB':
+            image = image.convert('RGB')
+
+        image = ImageOps.fit(image, (224,224), Image.Resampling.LANCZOS)
+        image_ = np.asarray(image)
+        # image = image / 255.0  # Normalize the pixel values
+        n_image = (image_.astype(np.float32) / 127.5) - 1
+        # image = np.expand_dims(image, axis=0)  # Add batch dimension
+
+        data[0] = n_image
+
+        # Make predictions with your model.
+        predictions = model.predict(data)
+
+        predicted_class_index = np.argmax(predictions)
+
+        predicted_class_name = class_labels[predicted_class_index]
+
+        confidence = float(predictions[0][predicted_class_index])
+
+        response = {
+            'predicted_class': predicted_class_name,
+            'confidence': confidence,
+        }
+
+
+        return jsonify(response)
+
+    except Exception as e:
+        print(f"AlzheimerError: {str(e)}")
+        return jsonify({'error': str(e)}), 500
+ 
+
+
 @app.route('/predict/<string:img>', methods=['POST'])
 def predict(img):
     if(img == 'fundus'):
         response = predict_fundus()
     elif(img == 'xray'):
         response = predict_xray()
+    elif(img == 'alzheimer'):
+        response =predict_alzheimer()
     return response
 
 if __name__ == '__main__':
